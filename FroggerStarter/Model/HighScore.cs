40ea02936352fa09cacd21928b4c7083bb78b9b6
Jake Pefinis <jakepefinis25@gmail.com>
2019-11-10 21:16:00 -0500

    Added HighScore class and collection class, HighScoreBoard

---
+++ FroggerStarter/Model/HighScore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FroggerStarter.Model
+{
+    class HighScore
+    {
+        public String Name { get; }
+
+        public int Score { get; }
+
+        public int Level { get;  }
+
+        public HighScore(String name, int score, int level)
+        {
+            this.Name = name;
+            this.Score = score;
+            this.Level = level;
+        }
+    }
+}
---
+++ FroggerStarter/Model/HighScoreBoard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FroggerStarter.Model
+{
+    class HighScoreBoard
+    {
+        IList<HighScore> highScores;
+
+        public HighScoreBoard()
+        {
+            highScores = new List<HighScore>();
+        }
+
+        public void AddHighScore(HighScore score)
+        {
+            this.highScores.Add(score);
+        }
+    }
+}
