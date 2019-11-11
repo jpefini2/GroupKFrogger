9fd5564cafbd768e2e8fc14ff2da1842b35c5fe7
Jake Pefinis <jakepefinis25@gmail.com>
2019-11-11 03:30:21 -0500

    Re-added HighScoreElement enum and high scores text file

--- FroggerStarter/Model/HighScoreElement.cs
+++ FroggerStarter/Model/HighScoreElement.cs
@@ -1,15 +1,17 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace FroggerStarter.Model
 {
-    /// <summary>The type of vehicle</summary>
     public enum HighScoreElement
     {
-        /// <summary>The player's name</summary>
         Name,
-        /// <summary>The player's score</summary>
+
         Score,
-        /// <summary>The level the player achieved</summary>
+
         Level
     }
 }
